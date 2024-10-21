ceefe2a7f24f9193e1e287e614c6916b4d300d16
dominicmanini <46791828+dominicmanini@users.noreply.github.com>
2024-10-21 16:46:36 +0100

    Added Algorithm Unit Tests
    Added Unit Tests to check that the brute force and recursion algorithms provided the expected output.

--- OBM Technical Interview/OBM_Technical_Assessment/src/test/java/com/example/obm_technical_assessment/ObmTechnicalAssessmentApplicationTests.java
+++ OBM Technical Interview/OBM_Technical_Assessment/src/test/java/com/example/obm_technical_assessment/ObmTechnicalAssessmentApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.obm_technical_assessment;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openapitools.model.Algorithms.BRUTE_FORCE;
+import static org.openapitools.model.Algorithms.RECURSIVE;
+
+import com.example.obm_technical_assessment.service.PrimeService;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.openapitools.model.Algorithms;
+import org.openapitools.model.Prime;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class ObmTechnicalAssessmentApplicationTests {
 
   @Test
-  void contextLoads() {
+  void testBruteForce() {
+    PrimeService primeService = new PrimeService();
+    List<Integer> expectedInput = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19);
+    Prime expectedPrime = new Prime().primeNumbers(expectedInput);
+    assertEquals(expectedPrime, primeService.getPrimes(19, BRUTE_FORCE));
+
   }
 
+  @Test
+  void testRecursion() {
+    PrimeService primeService = new PrimeService();
+    List<Integer> expectedInput = Arrays.asList(19, 17, 13, 11, 7, 5, 3, 2);
+    Prime expectedPrime = new Prime().primeNumbers(expectedInput);
+    assertEquals(expectedPrime, primeService.getPrimes(19, RECURSIVE));
+  }
 }
